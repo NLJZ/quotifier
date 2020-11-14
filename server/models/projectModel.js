f22ab8dd07f38bbd5bc0a1b7d483739e38cc1c57
NLJZ <johnson.nathan.lee@gmail.com>
2020-11-14 09:27:52 +0100

    project model added

---
+++ server/models/projectModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const projectSchema = Schema(
+  {
+    _id: Schema.Types.ObjectId,
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    quotes: {
+      type: Array,
+      default: undefined,
+    },
+  },
+  { timestamps: true }
+);
+
+// static method to make sure object exists
+projectsSchema.statics.projectCheck = async function (id) {
+  try {
+    const project = await this.findById(id);
+    if (project) {
+      return project;
+    }
+    throw Error("Project Does Not Exist");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const Project = mongoose.model("Project", projectSchema);
+module.exports = Project;
