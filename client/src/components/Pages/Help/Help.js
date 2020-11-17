1a019a9900f7880a592a378732603438583d98ca
NLJZ <johnson.nathan.lee@gmail.com>
2020-11-17 09:51:34 +0100

    sort state by date

--- client/src/components/Pages/Help/Help.js
+++ client/src/components/Pages/Help/Help.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { getAllTags, findQuotesByTag } from "../../../helpers/sortSearch.js";
+import {
+  getAllTags,
+  findQuotesByTag,
+  sortNewToOld,
+} from "../../../helpers/sortSearch.js";
 
 function Help() {
   const quotes = useSelector((state) => state.quotes);
   const tags = getAllTags(quotes);
   const quotesWithTag = findQuotesByTag(
-    ["pessimism", "Dogs", "test test test"],
+    ["pessimism", "Dogs", "test TEST tesT"],
     quotes
   );
+  const sortedQuotesByDate = sortNewToOld(quotes);
   console.log(tags);
   console.log(quotesWithTag);
+  console.log(sortedQuotesByDate);
   return (
     <div className="help">
       <h2>help</h2>
--- client/src/helpers/sortSearch.js
+++ client/src/helpers/sortSearch.js
@@ -13,17 +13,30 @@ export const getAllTags = (quotesState) => {
   return tagArray;
 };
 
-// pass an array of quotes and the state of quotes (from redux) to find all quotes with any of the tags
+// pass an array of quotes and the state of quotes (from redux) to find all quotes including any one of the tags
 export const findQuotesByTag = (array, quotesState) => {
   const quoteArray = [];
   const quoteStateArray = Object.values(quotesState);
-  const tagArray = array;
+  const tagArray = array.map((tag) => tag.toLowerCase());
   tagArray.forEach((tag) => {
     quoteStateArray.forEach((quote) => {
-      if (quote.tags !== undefined && quote.tags.includes(tag)) {
-        quoteArray.push(quote);
+      if (quote.tags !== undefined) {
+        const tagsToLower = quote.tags.map((value) => value.toLowerCase());
+        if (tagsToLower.includes(tag)) {
+          quoteArray.push(quote);
+        }
       }
     });
   });
   return quoteArray;
+};
+
+export const sortNewToOld = (state) => {
+  const stateArray = Object.values(state);
+  const sorted = stateArray.sort((a, b) => {
+    let dateA = new Date(a.createdAt);
+    let dateB = new Date(b.createdAt);
+    return dateB - dateA;
+  });
+  return sorted;
 };
