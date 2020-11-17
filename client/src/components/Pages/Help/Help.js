3ac056f923d6a61c581e1c553ff9f253762c3266
NLJZ <johnson.nathan.lee@gmail.com>
2020-11-17 08:09:05 +0100

    find by quotes by tag works

--- client/src/components/Pages/Help/Help.js
+++ client/src/components/Pages/Help/Help.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { getAllTags } from "../../../helpers/sortSearch.js";
+import { getAllTags, findQuotesByTag } from "../../../helpers/sortSearch.js";
 
 function Help() {
   const quotes = useSelector((state) => state.quotes);
   const tags = getAllTags(quotes);
+  const quotesWithTag = findQuotesByTag(
+    ["pessimism", "Dogs", "test test test"],
+    quotes
+  );
   console.log(tags);
+  console.log(quotesWithTag);
   return (
     <div className="help">
       <h2>help</h2>
--- client/src/helpers/sortSearch.js
+++ client/src/helpers/sortSearch.js
@@ -16,5 +16,14 @@ export const getAllTags = (quotesState) => {
 // pass an array of quotes and the state of quotes (from redux) to find all quotes with any of the tags
 export const findQuotesByTag = (array, quotesState) => {
   const quoteArray = [];
+  const quoteStateArray = Object.values(quotesState);
   const tagArray = array;
+  tagArray.forEach((tag) => {
+    quoteStateArray.forEach((quote) => {
+      if (quote.tags !== undefined && quote.tags.includes(tag)) {
+        quoteArray.push(quote);
+      }
+    });
+  });
+  return quoteArray;
 };
