ba728e5b1884e7424e8db3f6ee98b2ae6e057a55
NLJZ <johnson.nathan.lee@gmail.com>
2020-11-21 18:47:20 +0100

    alpha tags

--- client/src/redux/reducers/tagsReducer.js
+++ client/src/redux/reducers/tagsReducer.js
@@ -4,7 +4,7 @@ import produce from "immer";
 const favesReducer = (state = [], action) => {
   switch (action.type) {
     case "LOAD_TAGS":
-      const tagsArr = getAllTags(action.payload);
+      const tagsArr = getAllTags(action.payload).sort();
       return tagsArr;
     default:
       return state;
