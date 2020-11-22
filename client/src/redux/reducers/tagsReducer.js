091e962a9f1c5738dd8c4f1c1a684d008cac6dbf
NLJZ <johnson.nathan.lee@gmail.com>
2020-11-22 08:53:41 +0100

    case insensitive alpha tags

--- client/src/redux/reducers/tagsReducer.js
+++ client/src/redux/reducers/tagsReducer.js
@@ -4,7 +4,9 @@ import produce from "immer";
 const favesReducer = (state = [], action) => {
   switch (action.type) {
     case "LOAD_TAGS":
-      const tagsArr = getAllTags(action.payload).sort();
+      const tagsArr = getAllTags(action.payload).sort(function (a, b) {
+        return a.toLowerCase().localeCompare(b.toLowerCase());
+      });
       return tagsArr;
     default:
       return state;
