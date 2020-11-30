672bfdf5684f8ba2c4603f9d4f8e3ca217ed028c
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-11-30 09:17:02 +0100

    quotifier onclick

--- client/src/components/Nav/NavLogin.js
+++ client/src/components/Nav/NavLogin.js
@@ -1,15 +1,27 @@
 import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import NavLoginIconAvatar from "./NavLoginIconAvatar";
 import NavLoginIconThreeDots from "./NavLoginIconThreeDots";
 import NavLoginIconResponsive from "./NavLoginIconResponsive";
 
 function NavLogin() {
+  const quoteViewerOn = useSelector((state) => state.quoteViewer);
+
+  const [showGreeting, setShowGreeting] = useState(quoteViewerOn);
+  console.log(showGreeting);
   const [width, setWidth] = useState(window.InnerWidth);
   const breakPoint = 800;
 
   useEffect(() => {
     window.addEventListener("resize", () => setWidth(window.innerWidth));
   }, []);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setShowGreeting(!quoteViewerOn);
+    console.log("quoteViewerOff");
+  };
+
   return (
     <div className="nav-container-login">
       {width < breakPoint ? (
@@ -17,7 +29,9 @@ function NavLogin() {
           <NavLoginIconResponsive />
         </div>
       ) : (
-        <p className="nav-logo">"quotifier"</p>
+        <button className="nav-logo-button" onClick={handleChange}>
+          <p className="nav-logo">"quotifier"</p>
+        </button>
       )}
 
       <div className="menu-top-icons ">
