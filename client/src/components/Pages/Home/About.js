1bf520385516b8089de34aeceef05e6ca875422c
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-11-30 13:11:38 +0100

    landing page merrier

--- client/src/components/Pages/Home/About.js
+++ client/src/components/Pages/Home/About.js
@@ -2,14 +2,22 @@ import React, { useEffect } from "react";
 //-----animations on scrolling-----------------------
 import Aos from "aos";
 import "aos/dist/aos.css";
+//----------------------
+import Background from "./img/booksstapple.jpg";
 
 function About() {
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
 
+  const backgroundImage = {
+    width: "100%",
+    height: "400px",
+    backgroundImage: "url(" + { Background } + ")",
+  };
+
   return (
-    <section className="about section-scroll">
+    <section className="about section-scroll" style={backgroundImage}>
       <div className="home-element-content" data-aos="zoom-in">
         <h2 className="home-header">Welcome!</h2>
         <p className="text">
